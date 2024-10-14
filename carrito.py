79cea4e26b8dd74a58306d31dd08b1bac7352624
ch1llc0d3 <chillcrib7@gmail.com>
2024-10-14 18:59:16 -0300

    carrito y tienda

--- carrito.py
+++ carrito.py
@@ -1,4 +1,20 @@
 class Carrito:
     def __init__(self):
         self._productos = []
+    
+    def add_producto(self, producto):
+        self._productos.append(producto)
+        print(f"{producto._nombre} ha sido add al carrito")
         
+    def ver_carrito(self):
+        if not self._productos:
+            print("No hay productos")
+        else:
+            print("Productos en el carrito:")
+            for producto in self._productos:
+                print(producto.ver_info())
+                
+    def calcular_total(self):
+        total = sum(producto.ver_precio() for producto in self._productos)
+        print(f"EL total a pagar es: ${total}")
+        return total 
---
+++ tienda.py
@@ -0,0 +1,5 @@
+class Tienda:
+    def __init__(self, nombre):
+        self._hombre = nombre
+        self._productos = []
+        
