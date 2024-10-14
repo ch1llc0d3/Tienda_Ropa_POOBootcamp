62e6e9b94c2e8a6a9010ca5306c048f5584f5bba
ch1llc0d3 <chillcrib7@gmail.com>
2024-10-14 19:03:18 -0300

    carrito, tienda, producto

--- tienda.py
+++ tienda.py
@@ -3,3 +3,13 @@ class Tienda:
         self._hombre = nombre
         self._productos = []
         
+    def add_producto(self, producto):
+        self._productos.append(producto)
+        
+    def ver_productos(self):
+        for i, producto in enumerate(self._productos, 1):
+            print(f"{1}. {producto.ver_info()}")
+    
+    def select_producto(self, indice):
+        return self._productos[indice - 1]
+    
